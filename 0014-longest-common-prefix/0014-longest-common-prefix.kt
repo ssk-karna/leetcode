e381a1ff29546f7a80a6df8ac5a317355e06900d
Satyam Shashank Karna <64829312+ssk-karna@users.noreply.github.com>
2024-12-25 11:20:09 +0530

    Time: 8 ms (64.66%), Space: 38.2 MB (37.37%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.kt
+++ 0014-longest-common-prefix/0014-longest-common-prefix.kt
@@ -1,16 +1,17 @@
 class Solution {
     fun longestCommonPrefix(strs: Array<String>): String {
-        
-        if(strs.isEmpty()) return ""
         var prefix = strs[0]
-      
-        for(i in 1 until strs.size){
-            while(strs[i].indexOf(prefix) != 0){
-                prefix = prefix.substring(0, prefix.length - 1)
-                if (prefix.isEmpty()) return ""
-            }
+
+    if (strs.isEmpty()) return ""
+
+    for (i in 1 until strs.size){
+
+        while (strs[i].indexOf(prefix) != 0){
+            prefix = prefix.substring(0, prefix.length - 1)
+            if (prefix.isEmpty()) return  ""
         }
-        
-        return prefix
+    }
+
+    return prefix
     }
 }
