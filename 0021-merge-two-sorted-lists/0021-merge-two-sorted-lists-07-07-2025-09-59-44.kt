80ec7222b51a4a1193e6b088854393cbf1a4bc3a
Satyam Shashank Karna <64829312+ssk-karna@users.noreply.github.com>
2025-07-07 09:59:44 +0530

    1 ms (20.58%) 43.3 MB (11.57%) 0021-merge-two-sorted-lists .kt

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists-07-07-2025-09-59-44.kt
@@ -0,0 +1,35 @@
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+class Solution {
+    fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
+        val dummy = ListNode(0)  // Dummy node to simplify edge cases
+    var tail = dummy
+
+    var l1 = list1
+    var l2 = list2
+
+    while (l1 != null && l2 != null) {
+        if (l1.`val` <= l2.`val`) {
+            tail.next = l1
+            l1 = l1.next
+        } else {
+            tail.next = l2
+            l2 = l2.next
+        }
+        tail = tail.next!!
+    }
+
+    // Attach the remaining part
+    tail.next = l1 ?: l2
+
+    return dummy.next
+    }
+}
+
